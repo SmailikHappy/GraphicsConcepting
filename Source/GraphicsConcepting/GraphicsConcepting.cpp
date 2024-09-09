9bfdece3803f462f56995e22f5dad69f6c8acfd0
SmailikHappy <224075@buas.nl>
2024-09-09 12:26:00 +0300

    Initial

---
+++ Source/GraphicsConcepting/GraphicsConcepting.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "GraphicsConcepting.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GraphicsConcepting, "GraphicsConcepting" );
